19e22d326380fea22918f966d9e254f192e72791
zetta102 <jsierra669@gmail.com>
2021-10-02 19:00:00 -0300

    Add animation imports

--- app/src/main/java/com/example/nxappc/ui/composables/NavigationController.kt
+++ app/src/main/java/com/example/nxappc/ui/composables/NavigationController.kt
@@ -2,12 +2,12 @@ package com.example.nxappc.ui.composables
 
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.composable
 import coil.annotation.ExperimentalCoilApi
 import com.example.nxappc.ui.objects.Routes
 import com.example.nxappc.ui.state.ProductModel
 import com.example.nxappc.ui.views.*
 import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
 @ExperimentalAnimationApi
